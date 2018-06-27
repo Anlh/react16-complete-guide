fd0fb937dfc3b55cbe9315436af7e083b70dfd1e
Helder Ferreira <helder.ferreira@pingpost.pt>
2018-06-27 14:17:00 +0100

    -> Stateless Person.js vs stateful App components

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -6,10 +6,18 @@ class App extends Component {
     state = {
         name: 'Tiago'
     };
+
+    // Using the arrow syntax we can pass the function directly on the event click
+    // this keyword will reference the App class and not to the event context
+    // if we had use a normal function this would emit an error
+    switchNameHandler = () => {
+        this.setState({name: 'Helder'});
+    };
+
     render() {
         return (
             <div className="App">
-                <button onClick={() => this.setState({name: 'Helder'})}>Change name</button>
+                <button onClick={this.switchNameHandler}>Change name</button>
                 <Person name={this.state.name}> My hobbies </Person>
             </div>
         );
