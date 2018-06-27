3c14edde011b18c012e6bbf222ef03ab93abc369
Helder Ferreira <helder.ferreira@pingpost.pt>
2018-06-27 13:38:55 +0100

    -> Created functional component/dummy component -> Output dynamic content using the {} syntax -> Work with props and state

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,11 +1,16 @@
 import React, {Component} from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
+    state = {
+        name: 'Tiago'
+    };
     render() {
         return (
             <div className="App">
-                <h1>Hello React</h1>
+                <button onClick={() => this.setState({name: 'Helder'})}>Change name</button>
+                <Person name={this.state.name}> My hobbies </Person>
             </div>
         );
     }
