71b19860fb15a98d2624ffa2d6206ba10f21fd25
Helder Ferreira <helder.ferreira@pingpost.pt>
2018-06-27 15:10:31 +0100

    -> Passing method references to the App child components

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -4,21 +4,36 @@ import Person from './Person/Person';
 
 class App extends Component {
     state = {
-        name: 'Tiago'
+        persons: [
+            {name: 'Pedro'},
+            {name: 'Daniel'}
+        ]
     };
 
     // Using the arrow syntax we can pass the function directly on the event click
     // this keyword will reference the App class and not to the event context
     // if we had use a normal function this would emit an error
-    switchNameHandler = () => {
-        this.setState({name: 'Helder'});
+    switchNameHandler = (newName = 'Helder') => {
+        this.setState({
+            persons: [
+                {name: newName},
+                {name: 'Tiago'}
+            ]
+        });
     };
+
+
 
     render() {
         return (
             <div className="App">
-                <button onClick={this.switchNameHandler}>Change name</button>
-                <Person name={this.state.name}> My hobbies </Person>
+                {/*Be aware This is cost inefficient than the bind syntax,
+                Because the DOM can be updated many times*/}
+                <button onClick={() => this.switchNameHandler()}>Change name</button>
+                <Person name={this.state.persons[0].name} />
+                <Person
+                    click={this.switchNameHandler.bind(this, 'Rui')}
+                    name={this.state.persons[1].name}/>
             </div>
         );
     }
