aa71e6c364dfd4e7798ac31ef411bad30c76545a
Helder Ferreira <helder.ferreira@pingpost.pt>
2018-06-27 16:48:09 +0100

    Add external and inline styles in React

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -5,44 +5,35 @@ import Person from './Person/Person';
 class App extends Component {
     state = {
         persons: [
-            {name: ''},
             {name: 'Daniel'}
         ]
-    };
-
-    // Using the arrow syntax we can pass the function directly on the event click
-    // this keyword will reference the App class and not to the event context
-    // if we had use a normal function this would emit an error
-    switchNameHandler = (newName = 'Helder') => {
-        this.setState({
-            persons: [
-                {name: newName},
-                {name: 'Daniel'}
-            ]
-        });
     };
 
     changeNameHandler = (newName) => {
         this.setState({
             persons: [
                 {name: newName},
-                {name: 'Daniel'}
             ]
         });
     };
 
     render() {
+        const btnStyle = {
+            backgroundColor: 'white',
+            font: 'inherit',
+            border: '1px solid blue',
+            padding: '8px',
+            cursor: 'pointer'
+        };
+
         return (
             <div className="App">
                 {/*Be aware This is cost inefficient than the bind syntax,
                 Because the DOM can be updated many times*/}
-                <button onClick={() => this.switchNameHandler()}>Change name</button>
+                <button style={btnStyle}>Button to style</button>
                 <Person
                     change={this.changeNameHandler.bind(this)}
                     name={this.state.persons[0].name} />
-                <Person
-                    click={this.switchNameHandler.bind(this, 'Rui')}
-                    name={this.state.persons[1].name}/>
             </div>
         );
     }
--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
+import './Person.css';
+
 const person = (props) => {
     const input = props.change ? <input type="text" onChange={(event) => props.change(event.target.value)}/> : null;
     return (
-        <div>
+        <div className="Person">
             <p onClick={props.click}>{props.name}</p>
             <p>{props.children}</p>
             {input}
