52096da114848ea970c631eddeb117bfa792b100
Helder Ferreira <helder.ferreira@pingpost.pt>
2018-06-28 15:39:07 +0100

    -> Replace radium library with css modules

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,6 +1,4 @@
 import React, {Component} from 'react';
-import Radium, {StyleRoot} from 'radium'; // In order to use media queries with the Radium we must wrap our Root component with StyleRoot
-
 import './App.css';
 import Person from './components/Person';
 
@@ -31,11 +29,7 @@ class App extends Component {
             font: 'inherit',
             border: '1px solid blue',
             padding: '8px',
-            cursor: 'pointer',
-            ':hover': {
-                backgroundColor: 'lightgreen',
-                color: 'black'
-            }
+            cursor: 'pointer'
         };
         let classes = ['bold', 'red'].join(' ');
 
@@ -49,22 +43,15 @@ class App extends Component {
             });
 
             style.backgroundColor = 'red';
-
-            style[':hover'] = {
-                backgroundColor: 'lightred',
-                color: 'black'
-            }
         }
 
         return (
-            <StyleRoot>
-                <div className="App">
-                    <h1>React app</h1>
-                    <p className={classes}>List of persons</p>
-                    <button style={style} onClick={() => this.togglePersons()}>Toggle Persons</button>
-                    {personsList}
-                </div>
-            </StyleRoot>
+            <div className="App">
+                <h1>React app</h1>
+                <p className={classes}>List of persons</p>
+                <button style={style} onClick={() => this.togglePersons()}>Toggle Persons</button>
+                {personsList}
+            </div>
         );
     }
 }
--- react-complete-guide/src/components/Person.js
+++ react-complete-guide/src/components/Person.js
@@ -1,16 +1,11 @@
 import React from 'react';
-import Radium from 'radium';
 
 import './Person.css';
 
 const person = (props) => {
-    const style = {
-        '@media (min-width: 500px)': {
-            width: '450px'
-        }
-    };
+
     return (
-        <div className="Person" style={style} onClick={props.click}>
+        <div className="Person" onClick={props.click}>
             Name: {props.name}
             <br/>
             Age: {props.age}
@@ -18,4 +13,4 @@ const person = (props) => {
     );
 };
 
-export default Radium(person);
+export default person;
